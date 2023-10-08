d899b9192bd61130e7ee3c13ea68e764678cc35b
Lius <142863069+noturnachs@users.noreply.github.com>
2023-10-08 14:52:28 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+import NavBar from "./components/NavBar";
+import Body from "./components/Body";
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <Body />
+    </>
+  );
+};
+
+export default App;
